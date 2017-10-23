35bd24794ae7e2304bffe6f00ff33a44ac16b3a8
Lantianboy <1044722126@qq.com>
2017-10-23 16:27:35 +0800

    创建一个nib

---
+++ wwwww/wwwww/ViewXib.h
@@ -0,0 +1,13 @@
+//
+//  ViewXib.h
+//  wwwww
+//
+//  Created by 最爱是深蓝 on 2017/10/23.
+//  Copyright © 2017年 ProductTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewXib : UIView
+
+@end
---
+++ wwwww/wwwww/ViewXib.m
@@ -0,0 +1,21 @@
+//
+//  ViewXib.m
+//  wwwww
+//
+//  Created by 最爱是深蓝 on 2017/10/23.
+//  Copyright © 2017年 ProductTest. All rights reserved.
+//
+
+#import "ViewXib.h"
+
+@implementation ViewXib
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
